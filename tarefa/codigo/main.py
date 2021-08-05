79f34b7858ba323a35041d841604688e1ac68ba5
Alex Rocha <alexjorocha@gmail.com>
2021-08-05 15:44:52 -0400

    Atividade do dia 04/07 do MaratonaIA

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -4,10 +4,28 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
+async def titanic(Sex: int, Age: float, Lifeboat: int, Pclass: int):
+    with open('model/Titanic.pkl', 'rb') as fid:
         titanic = pickle.load(fid)
 
-@app.get('/model')
-def get():
-    return {'hello':'test'}
+    previsao = titanic.predict([[Sex, Age, Lifeboat, Pclass]]).tolist()
+    
+    surv = previsao[0]
+
+    try:
+        return {
+            'survived': surv,
+            'status': 200,
+            'message': 'Predição bem sucedida!',
+        }
+    except Exception:
+        return {
+            'message': 'Erro no servidor'
+        }
+
+
+#@app.get('/model')
+#async def get():
+#    return {
+#       'hello': 'test'
+#    }
